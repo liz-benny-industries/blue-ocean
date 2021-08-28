b5ffe34208aa03ee8577b48f7a7405a822aa8daa
rgcuffee <rgcuffee@gmail.com>
2021-08-27 18:52:31 -0700

    implemented firebase signing/signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Auth from './Components/Forms/Auth';
 
-const App = () => <div>Hello World!</div>;
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    console.log(uid)
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+const App = ({ uid }) => {
+  const [isAuthOpen, setAuthOpen] = useState(true);
+  return (
+    <div>
+      Hello World!
+      {uid}
+      <div
+        style={{ cursor: 'pointer' }}
+        onClick={signOut}
+      >SignOut</div>
+      <Auth
+        setAuthOpen={setAuthOpen}
+        isAuthOpen={isAuthOpen}
+      />
+    </div>
+  );
+};
 
 export default App;
