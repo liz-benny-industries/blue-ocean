e1a018ebbe0664c4f8069d10a18a56563d6d7977
rgcuffee <rgcuffee@gmail.com>
2021-08-27 20:42:27 -0700

    slashed out apiKey will make secret next

--- client/src/Components/Forms/Auth.jsx
+++ client/src/Components/Forms/Auth.jsx
@@ -10,10 +10,10 @@ import Button from '@material-ui/core/Button';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import Modal from '../Helpers/Modal';
-import { apiKey } from '../../../../config/config';
+// import { apiKey } from '../../../../';
 
 const firebaseConfig = {
-  apiKey,
+  // apiKey,
   authDomain: 'sandbox-3a2e3.firebaseapp.com',
   projectId: 'sandbox-3a2e3',
   storageBucket: 'sandbox-3a2e3.appspot.com',
