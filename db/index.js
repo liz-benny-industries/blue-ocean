3b41ae24859ebd53c707da61f4edd1da73af4271
adbarc92 <adbarclay92@gmail.com>
2021-08-30 16:58:24 -0600

    Destructures env

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 const { Sequelize } = require('sequelize');
-// const config = require('../config/config');
 require('dotenv').config();
 
+const {
+  DB_USER, DB_PASSWORD, DB_PORT, DB_NAME, DB_HOST
+} = process.env;
+
 const connection = new Sequelize(
-  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 );
 
 module.exports = connection;
