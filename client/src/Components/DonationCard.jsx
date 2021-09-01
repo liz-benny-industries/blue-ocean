97b65ccc635d07f8237b7fb2f01a03de033fae3d
Richard Cuffee <79486064+rgcuffee@users.noreply.github.com>
2021-08-31 18:57:52 -0700

    Update DonationCard.jsx

--- client/src/Components/DonationCard.jsx
+++ client/src/Components/DonationCard.jsx
@@ -1,6 +1,5 @@
 
 /* eslint-disable react/prop-types */
-=======
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
