3db22fe2a13f2e6e1d2a351eb4ec0a89ce3e2fbe
rgcuffee <rgcuffee@gmail.com>
2021-08-31 17:11:10 -0700

    example

---
+++ config/config.examlpe.js
@@ -0,0 +1,8 @@
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '';
+
+module.exports = {
+  CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN
+};
