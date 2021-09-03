dfd74aa76febbec9f5f6fb5722033c6d85ec74af
Richard Cuffee <79486064+rgcuffee@users.noreply.github.com>
2021-09-02 21:21:02 -0700

    Update Upload.jsx

--- client/src/Components/Upload.jsx
+++ client/src/Components/Upload.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useContext } from 'react';
 import AWS from 'aws-sdk';
 import AppContext from './context';
-import { accessKey, secretAccessKey } from '../../../config/config.js'
 const S3_BUCKET = 'blue-ocean-images';
 const REGION = 'us-east-2';
 
 AWS.config.update({
-  accessKeyId,
-  secretAccessKey,
+  accessKeyId: ''
+  secretAccessKey: ''
 });
 
 const myBucket = new AWS.S3({
